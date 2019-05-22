ed88ee9611c83d6888a66dd57913ef20dafc88b5
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-22 13:22:21 -0600

    Initial user story spec

---
+++ directions.md
@@ -0,0 +1,19 @@
+<!-- You are the new web developer for Hogwarts. The IT staff has provided an API where you can get a listing of students. 
+​
+`http://hogwarts-it.herokuapp.com`
+​
+You are to create an app and complete this following user story:
+​
+```
+As a user
+When I visit "/"
+And I select "Slytherin" from the dropdown
+And I click on "Get Students"
+Then my path should be "/search" with "house=slytherin" in the parameters
+And I should see a message "22 Students"
+And I should see a list of the 22 members of Slytherin
+And I should see a name and id for each student.
+```
+​
+​
+Your key is: alohamora -->
---
+++ spec/features/users/user_can_get_students_from_a_house_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+# `http://hogwarts-it.herokuapp.com`
+# Your key is: alohamora
+
+describe 'as a user on the home page' do
+  context 'when I select Slytherin from a drop down' do
+    it 'returns a list of those students' do
+      # As a user
+      # When I visit "/"
+      visit root_path
+      # And I select "Slytherin" from the dropdown
+      select('Slytherin', :from => 'house')
+      # And I click on "Get Students"
+      click_on 'Get Students'
+      # Then my path should be "/search" with "house=slytherin" in the parameters
+      expect(current_url).to eq(/search?house=slytherin)
+      # And I should see a message "22 Students"
+      expect(page).to have_content("22 Students")
+      # And I should see a list of the 22 members of Slytherin
+      expect(page).to have_css('.student', count: 22)
+      # And I should see a name and id for each student.
+      within first('.student') do
+        expect(page).to have_css('.name')
+        expect(page).to have_css('.id')
+      end
+    end
+  end
+end
